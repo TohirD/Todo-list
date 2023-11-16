caf17bd0b23d734082ad095f20ed7e0bfdbcfe12
TohirD <tokhir2004@gmail.com>
2023-11-17 00:51:21 +0200

    first upload

---
+++ src/components/EditTodoForm.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+export const EditTodoForm = ({editTodo, task}) => {
+  const [value, setValue] = useState("")
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    editTodo(value, task.id)
+    setValue("")
+  }
+
+  return (
+    <form className='TodoForm' onSubmit={handleSubmit}>
+      <input type="text" className='todo-input' placeholder='Update Task?' onChange={(e) => setValue(e.target.value)} value={value}/>
+      <button type='submit' className='todo-btn'>Update Task</button>
+    </form>
+  )
+}
+
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import '../Home/home.css'
+import { TodoWrapper } from '../TodoWrapper'
+
+export const Home = () => {
+  return (
+    <div className='Home'>
+        <TodoWrapper/>
+    </div>
+  )
+}
